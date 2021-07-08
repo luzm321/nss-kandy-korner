f749eaf3469066722c29c074efadbf2f5cb445b3
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-08 16:05:22 -0500

    initial commit; created components directory containing KandyKorner components and updated index.js to render the KandyKorner component

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./KandyKorner.css";
+
+export const KandyKorner = () => (
+    <>
+        <h2>Kandy Korner</h2>
+        <small>Indulge Your Cravings With Our Delicacies...</small>
+
+        <address className="address">
+            <div>Visit Us at the Nashville East Location</div>
+            <div>500 SweetTooth Way</div>
+        </address>
+
+        {/* <PropsAndState yourName={"Luz Angelique"} /> */}
+    </>
+)
