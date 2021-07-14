5a2b8ca78235428ec37d43f7911bdcc2057d0da0
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-07-14 14:27:21 -0500

    created auth directory containing Login and Register components that allow customers to sign in with their credentials or register an account; added styling to login and register components; refactored KandyKorner component to implement the login screen

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,12 +1,33 @@
 import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { ApplicationViews } from "./ApplicationViews.js";
+import { NavBar } from "./nav/NavBar.js";
+import { Login } from "./auth/Login.js";
+import { Register } from "./auth/Register.js";
 import "./KandyKorner.css";
-import { NavBar } from "./nav/NavBar.js"
-import { ApplicationViews } from "./ApplicationViews.js"
-
 
 export const KandyKorner = () => (
-    <>
-        <NavBar />
-        <ApplicationViews />
-    </>
+  <>
+    <Route
+      render={() => {
+        if (localStorage.getItem("kandy_customer")) {
+          return (
+            <>
+              <NavBar />
+              <ApplicationViews />
+            </>
+          );
+        } else {
+          return <Redirect to="/login" />;
+        }
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route path="/register">
+      <Register />
+    </Route>
+  </>
 );
